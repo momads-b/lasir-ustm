c64efb5487b5091d39343d9e45ebd067e9e29a0c
Sousa Junior <sousa.sj.sj@gmail.com>
2017-08-31 09:04:35 +0200

    Initial commit

---
+++ app/src/main/java/ustm/lasir2017/LasirActivity.java
@@ -0,0 +1,13 @@
+package ustm.lasir2017;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LasirActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lasir);
+    }
+}
